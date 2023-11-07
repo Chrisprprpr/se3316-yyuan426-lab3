25429d31ce400c5000e1717574a4589ebca1d3db
Chrisprprpr <yyuan426@uwo.ca>
2023-11-07 16:37:56 -0500

    Fix the path

--- server/index.js
+++ server/index.js
@@ -43,6 +43,11 @@ app.listen(port, () =>
     console.log(`Listening on port ${port}`)
 );
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
 // Route to get superheroes by name
 router.get('/getSuperheroByName/:name', (req, res) => {
